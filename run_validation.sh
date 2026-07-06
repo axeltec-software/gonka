1d22683d65c9c1f338e5dae3f132d676e487549a
Irene Mizus <irene.mizus@axel-t.com>
2026-07-06 10:42:16 +0200

    Checked the prefill-hack workflow. Fixed a bug.

--- run_validation.sh
+++ run_validation.sh
@@ -1,4 +1,4 @@
 python ./mlnode/packages/benchmarks/scripts/inference_validation/validation.py \
     --validation-url http://0.0.0.0:8005 \
     --validation-model Qwen/Qwen3-0.6B \
-    --inference-artifact "mlnode/packages/benchmarks/data/experiments/final_version/inference_fraud_hack/inference_results.jsonl"
+    --inference-artifact "mlnode/packages/benchmarks/data/experiments/inference_fraud_hack/inference_results.jsonl"
